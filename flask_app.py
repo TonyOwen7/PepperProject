c0f6c095ef0c12243dd4eeebb08f3558ee65ce64
TonyOwen7 <moubouyitonyowen@gmail.com>
2024-11-03 19:03:38 +0100

    Correction

--- flask_app.py
+++ flask_app.py
@@ -24,24 +24,23 @@ def init_db():
     cursor.execute('SELECT COUNT(*) FROM ros_commands')
     if cursor.fetchone()[0] == 0:
         commands = [
-            (1, 'Lancer DCM', 'Lance le DCM pour connecter Pepper via ROS avec IP.', 'roslaunch pepper_dcm_bringup pepper_bringup.launch robot_ip:=192.168.1.34 roscore_ip:=localhost network_interface:=enp0s3'),
-            (2, 'Driver Naoqi', 'Démarre le driver Naoqi pour interagir avec Pepper.', 'roslaunch naoqi_driver naoqi_driver.launch nao_ip:=192.168.0.126 roscore_ip:=localhost network_interface:=enp0s8'),
-            (3, 'Calibrer caméra', 'Lance l’outil de calibration de la caméra.', 'rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.108 image:=/cv_camera/image_raw camera:=/cv_camera'),
-            (4, 'Contrôle joystick', 'Permet de contrôler Pepper via une manette (joystick).', 'roslaunch teleop_twist_joy pepper.launch'),
+            (0, 'Lancer DCM', 'Lance le DCM pour connecter Pepper via ROS avec IP.', 'roslaunch pepper_dcm_bringup pepper_bringup.launch robot_ip:=192.168.1.34 roscore_ip:=localhost network_interface:=enp0s3'),
+            (1, 'Driver Naoqi', 'Démarre le driver Naoqi pour interagir avec Pepper.', 'roslaunch naoqi_driver naoqi_driver.launch nao_ip:=192.168.0.126 roscore_ip:=localhost network_interface:=enp0s8'),
+            (2, 'Calibrer caméra', 'Lance l’outil de calibration de la caméra.', 'rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.108 image:=/cv_camera/image_raw camera:=/cv_camera'),
+            (3, 'Contrôle joystick', 'Permet de contrôler Pepper via une manette (joystick).', 'roslaunch teleop_twist_joy pepper.launch'),
+            (4, 'stop', 'Arrête immédiatement tous les mouvements de Pepper.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0, 0, 0]" "[0, 0, 0]"'),
             (5, 'Avancer', 'Fait avancer Pepper en ligne droite.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0.5, 0, 0]" "[0, 0, 0]"'),
             (6, 'Reculer', 'Fait reculer Pepper en ligne droite.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[-0.5, 0, 0]" "[0, 0, 0]"'),
-            (7, 'Tourner à gauche', 'Fait tourner Pepper vers la gauche sur un angle donné.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0, 0, 0]" "[0, 0, 0.5]"'),
-            (8, 'Tourner à droite', 'Fait tourner Pepper vers la droite sur un angle donné.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0, 0, 0]" "[0, 0, -0.5]"'),
-            (9, 'Avancer vers la gauche', 'Fait avancer Pepper tout en se déplaçant vers la gauche.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0.5, 0.5, 0]" "[0, 0, 0]"'),
-            (10, 'Avancer vers la droite', 'Fait avancer Pepper tout en se déplaçant vers la droite.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0.5, -0.5, 0]" "[0, 0, 0]"'),
-            (11, 'Tourner sur soi-même vers la gauche', 'Fait tourner Pepper sur lui-même dans le sens anti-horaire (vers la gauche).', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0, 0, 0]" "[0, 0, 0.5]"'),
-            (12, 'Tourner sur soi-même vers la droite', 'Fait tourner Pepper sur lui-même dans le sens horaire (vers la droite).', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0, 0, 0]" "[0, 0, -0.5]"'),
+            (7, 'Aller à gauche', 'Déplace Pepper latéralement vers la gauche.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0, 0.5, 0]" "[0, 0, 0]"'),
+            (8, 'Aller à droite', 'Déplace Pepper latéralement vers la droite.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0, -0.5, 0]" "[0, 0, 0]"'),
+            (9, 'Avancer vers la gauche', 'Fait avancer Pepper en diagonale vers la gauche.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0.5, 0.5, 0]" "[0, 0, 0]"'),
+            (10, 'Avancer vers la droite', 'Fait avancer Pepper en diagonale vers la droite.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0.5, -0.5, 0]" "[0, 0, 0]"'),
+            (11, 'Reculer vers la gauche', 'Fait reculer Pepper en diagonale vers la gauche.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[-0.5, 0.5, 0]" "[0, 0, 0]"'),
+            (12, 'Reculer vers la droite', 'Fait reculer Pepper en diagonale vers la droite.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[-0.5, -0.5, 0]" "[0, 0, 0]"'),
             (13, 'Tour complet vers la gauche', 'Fait tourner Pepper sur lui-même à 360 degrés dans le sens anti-horaire.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0, 0, 0]" "[0, 0, 3.14]"'),
-            (14, 'Tour complet vers la droite', 'Fait tourner Pepper sur lui-même à 360 degrés dans le sens horaire.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0, 0, 0]" "[0, 0, -3.14]"'),
-            (15, 'Indiquer la position de la paume de la main', 'Retourne la position de la paume de la main en utilisant les joints associés.', 'rostopic echo /joint_states | grep LHand'),
-            (16, 'Parler', 'Fait dire un message prédéfini à Pepper.', 'rostopic pub /speech std_msgs/String "data: \'Hello ISTY students\'"'),
-            (17, 'Saluer', 'Fait lever la main de Pepper pour dire bonjour.', 'rostopic pub /joint_angles naoqi_bridge_msgs/JointAnglesWithSpeed "joint_names: [\'LShoulderRoll\', \'LShoulderPitch\'], joint_angles: [0, 1.57], speed: 0.1"')
+            (14, 'Tour complet vers la droite', 'Fait tourner Pepper sur lui-même à 360 degrés dans le sens horaire.', 'rostopic pub /cmd_vel geometry_msgs/Twist "[0, 0, 0]" "[0, 0, -3.14]"')
         ]
+
         cursor.executemany('INSERT INTO ros_commands (id, command_name, description, parameters) VALUES (?, ?, ?, ?)', commands)
         conn.commit()
     conn.close()
