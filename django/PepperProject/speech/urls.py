d885ac4920392a08081484be494bc940a740e497
MOUBOUYI BANDOU Tony Owen <tony-owen.moubouyi-bandou@ens.uvsq.fr>
2025-03-13 10:16:38 +0100

    before test

--- django/PepperProject/speech/urls.py
+++ django/PepperProject/speech/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('speech_list/', views.speech_list, name='speech_list'),
+    path('user_speeches/', views.speech_list, name='user_speeches'),
     path('create/', views.create_speech, name='create_speech'),
     path('edit/<int:speech_id>/', views.edit_speech, name='edit_speech'),
     path('delete/<int:speech_id>/', views.delete_speech, name='delete_speech'),
